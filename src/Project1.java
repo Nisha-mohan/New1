84354b51e71ed3de3825c3fb50795ad396b6f044
Nisha <nisha_89mohan@yahoo.co.in>
2023-12-17 11:20:45 +0530

    Initial commit

---
+++ src/Project1.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Project1
+{
+    public static void main(String[] args) {
+        System.out.println("I have done my first Java project");
+        int a=5,b=2;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
