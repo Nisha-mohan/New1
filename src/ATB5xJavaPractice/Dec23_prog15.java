399a00bea1b8e1f4002e069ffa50436605b38007
Nisha <nisha_89mohan@yahoo.co.in>
2023-12-23 22:17:50 +0530

    Prime number program using for loop - Dec 23

---
+++ src/ATB5xJavaPractice/Dec23_prog15.java
@@ -0,0 +1,26 @@
+package src.ATB5xJavaPractice;
+
+import java.util.Scanner;
+
+public class Dec23_prog15 {
+    public static void main(String[] args) {
+        //Verify if a number provided by user is prime number or not, using for loop
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int num = sc.nextInt();
+        int i=0;
+        if (num == 0)
+            System.out.println(num + " is not a prime number");
+        else {
+            for (i = 2; i < 9; i++) {
+                if ((num%i==0)&&(num!=i)) {
+                    System.out.println(num + " is not a prime number");
+                    break;
+                }
+            }
+            if (i==9)
+                System.out.println(num + " is a prime number");
+        }
+        sc.close();
+    }
+}
